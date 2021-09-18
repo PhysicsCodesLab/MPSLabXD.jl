e11b4e160e71ad286aa2c79b66fa21b2e44f5cf7
Documenter.jl <documenter@juliadocs.github.io>
2021-09-18 20:58:03 +0000

    build based on 636c1c6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MPSLabXD","category":"page"},{"location":"#MPSXD","page":"Home","title":"MPSXD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MPSLabXD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"write something here!","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MPSLabXD]","category":"page"}]
+}
